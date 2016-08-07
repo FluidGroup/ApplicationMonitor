98eed78e7c69835f08487a7e3960ee07922141e9
muukii <m@muukii.me>
2016-08-08 01:28:06 +0900

    Init commit

---
+++ ApplicationMonitor/Classes/UIViewController+ApplicationMonitor.swift
@@ -0,0 +1,78 @@
+// UIViewController+ApplicationMonitor.swift
+//
+// Copyright (c) 2016 muukii
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+extension UIViewController {
+    
+    override public static func initialize() {
+        if !didUIViewControllerInitialize {
+            replaceMethods()
+            didUIViewControllerInitialize = true
+        }
+    }
+    
+    private static func replaceMethods() {
+        
+        method_exchangeImplementations(
+            class_getInstanceMethod(self, #selector(UIViewController.viewDidLoad)),
+            class_getInstanceMethod(self, #selector(UIViewController.me_muukii_transition_monitor_viewDidLoad)))
+        
+        method_exchangeImplementations(
+            class_getInstanceMethod(self, #selector(UIViewController.viewWillAppear(_:))),
+            class_getInstanceMethod(self, #selector(UIViewController.me_muukii_transition_monitor_viewWillAppear(_:))))
+        
+        method_exchangeImplementations(
+            class_getInstanceMethod(self, #selector(UIViewController.viewDidAppear(_:))),
+            class_getInstanceMethod(self, #selector(UIViewController.me_muukii_transition_monitor_viewDidAppear(_:))))
+        
+        method_exchangeImplementations(
+            class_getInstanceMethod(self, #selector(UIViewController.viewWillDisappear(_:))),
+            class_getInstanceMethod(self, #selector(UIViewController.me_muukii_transition_monitor_viewWillDisappear(_:))))
+        
+        method_exchangeImplementations(
+            class_getInstanceMethod(self, #selector(UIViewController.viewDidDisappear(_:))),
+            class_getInstanceMethod(self, #selector(UIViewController.me_muukii_transition_monitor_viewDidDisappear(_:))))
+    }
+    
+    @objc private func me_muukii_transition_monitor_viewDidLoad() {
+        ApplicationMonitor.viewControllerMonitorReport?.viewDidLoad(self)
+    }
+    
+    @objc private func me_muukii_transition_monitor_viewWillAppear(animated: Bool) {
+        ApplicationMonitor.viewControllerMonitorReport?.viewWillAppear(self)
+    }
+    
+    @objc private func me_muukii_transition_monitor_viewDidAppear(animated: Bool) {
+        ApplicationMonitor.viewControllerMonitorReport?.viewDidAppear(self)
+    }
+    
+    @objc private func me_muukii_transition_monitor_viewWillDisappear(animated: Bool) {
+        ApplicationMonitor.viewControllerMonitorReport?.viewWillDisappear(self)
+    }
+    
+    @objc private func me_muukii_transition_monitor_viewDidDisappear(animated: Bool) {
+        ApplicationMonitor.viewControllerMonitorReport?.viewDidDisappear(self)
+    }
+}
+
+private var didUIViewControllerInitialize: Bool = false
